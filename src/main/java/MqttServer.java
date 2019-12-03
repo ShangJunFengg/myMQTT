9313defb3b8ff3efc5a4122816713675f61b1de2
猪皮恶霸 <214529167@qq.com>
2019-12-03 11:07:47 +0800

    init

---
+++ src/main/java/MqttServer.java
@@ -0,0 +1,53 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.mqtt.MqttDecoder;
+import io.netty.handler.codec.mqtt.MqttEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+
+public class MqttServer {
+
+    public static void main(String[] args) throws Exception  {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                workerGroup.shutdownGracefully();
+                bossGroup.shutdownGracefully();
+            }
+        });
+
+
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) throws Exception {
+
+                        ChannelPipeline p = ch.pipeline();
+
+                        p.addFirst("idleHandler", new IdleStateHandler(0, 0, 120));
+                        p.addLast("encoder", MqttEncoder.INSTANCE);
+                        p.addLast("decoder", new MqttDecoder());
+                        p.addLast("logicHandler", new BrokerHandler(65535));
+
+                    }
+                })
+                .option(ChannelOption.SO_BACKLOG, 511)
+                .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+        ChannelFuture f = b.bind("0.0.0.0", 1883).sync();
+
+        f.channel().closeFuture().sync();
+    }
+
+}
